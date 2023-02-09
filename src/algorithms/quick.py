fe809e5653a20b95b4e05113af2507657cea461a
Matthew Roberts <matt.roberts4000@gmail.com>
2023-02-09 12:45:30 +0000

    feat: added quick sort

---
+++ src/algorithms/quick.py
@@ -0,0 +1,30 @@
+
+# Quick Sort
+
+# Best Runtime:     Ω(nlogn)
+# Avg. Runtime:     Θ(nlogn)
+# Worst Runtime:    O(n²)
+
+
+def partition(array, start, end):
+    pivot = start
+    for i in range(start+1, end+1):
+        if array[i] <= array[start]:
+            pivot += 1
+            array[i], array[pivot] = array[pivot], array[i]
+    array[pivot], array[start] = array[start], array[pivot]
+    return pivot
+
+
+
+def sort(array, start=0, end=None):
+    if end is None:
+        end = len(array) - 1
+    def quicksort(array, start, end):
+        if start >= end:
+            return
+        pivot = partition(array, start, end)
+        quicksort(array, start, pivot-1)
+        quicksort(array, pivot+1, end)
+        return array
+    return quicksort(array, start, end)
--- src/main.py
+++ src/main.py
@@ -2,12 +2,14 @@ import seed
 import algorithms.bubble
 import algorithms.selection
 import algorithms.insertion
+import algorithms.quick
 
 data = seed.generateRandomArray(15)
 
 print(data)
 print("----" * len(data))
 
-print(algorithms.bubble.sort(data))
-print(algorithms.selection.sort(data))
-print(algorithms.insertion.sort(data))
+print("Bubble:   ", algorithms.bubble.sort(data))
+print("Selection:", algorithms.selection.sort(data))
+print("Insertion:", algorithms.insertion.sort(data))
+print("Quick:    ", algorithms.quick.sort(data))
